31edcdc6f7ef073b15680e5c1acf4194a3058135
thakuranubhav <thakuranubhav867@gamil.com>
2025-12-10 12:57:56 +0530

    chore: add assets folder for images and static files

--- app/page.tsx
+++ app/page.tsx
@@ -9,30 +9,6 @@ import Services from "./components/Services";
 import Work from "./components/Work";
 
 export default function Home() {
-  const [isDarkMode,setIsDark]=useState(false);
-  
-  useEffect(()=>{
-    if(localStorage.theme === 'dark' || (!('theme'  in localStorage) && window.matchMedia('prefers-color-scheme:dark').matches)){
-      setIsDark(true)
-
-    } else{
-      setIsDark(false)
-
-    }
-
-  },[])
-
-  useEffect(()=>{
-    if(isDarkMode){
-      document.documentElement.classList.add('dark');
-      localStorage.theme= 'dark';
-    }
-    else{
-      document.documentElement.classList.remove('dark');
-      localStorage.theme = '';
-    }
-
-  },[isDarkMode])
 
   return (
    <>
